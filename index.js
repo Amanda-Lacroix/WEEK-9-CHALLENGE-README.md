97149ab6c409fb8a1f7cc26a94e012d4f287bccb
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-03-24 21:19:56 -0300

    added license list to questions array

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const fs = require(`fs`);
+const inquirer = require(`inquirer`);
+// Import module created to generate the README template
+const generateReadme = require(`./generateMarkdown`);
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Write the README.md
+fs.writeFile('README.md', generateReadme)
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Array of questions for user input into the README.md (lesson 19)
+const questions = [
+    {
+        name: `Title`,
+        type: `input`,
+        message: "What is the title of your project?"
+    
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        name: `License`,
+        type: `list`,
+        message: "Choose your license:",
+        choices: [`Apache`,`Eclipse`,`GNU`,`MIT`]
+    
+    },
+ 
+    {
+        name: `Description`,
+        type: `input`,
+        message: "Provide details of the project?"
+    
+    },
+    {
+        name: `Installation`,
+        type: `input`,
+        message: "How do you install this application?"
+    
+    },
+    {
+        name: `Usage`,
+        type: `input`,
+        message: "What is this application used for?"
+    
+    },
+    {
+        name: `Contibution`,
+        type: `input`,
+        message: "Outline resources used:"
+    
+    },
+    {
+        name: `Tests`,
+        type: `input`,
+        message: "?????:"
+    
+    },
+ 
+  ]
 
-// Function call to initialize app
-init();
+
+//Appends the answers to the questions to the README.md file
+// questions.then((prompt.answer),=>
+// console.log(answer);
+// fs.appendFile('README.md));
+
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
