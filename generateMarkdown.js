b6ea325e407f323ceaff2d5f7a2c353f68849df7
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-03-31 12:34:18 -0300

    adding license badge/links etc  info to generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,16 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+ switch(choices) {
+    case'Apache':
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+
+    case 'Eclipse':
+        return "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)]";
+
+    case 'GNU':
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)]";
+
+    case 'MIT':
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+
+    default:
+ }
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch(choices) {
+       case'Apache':
+           return "[![License](https://opensource.org/licenses/Apache-2.0)]";
+   
+       case 'Eclipse':
+           return "[![License](https://opensource.org/licenses/EPL-1.0)]";
+   
+       case 'GNU':
+           return "[![License: GPL v3](http://www.gnu.org/licenses/gpl-3.0)]";
+   
+       case 'MIT':
+           return "[![License: MIT](https://opensource.org/licenses/MIT)]";
+   
+       default:
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateReadme(answers){
--- index.js
+++ index.js
@@ -44,21 +44,8 @@ inquirer
         name: `License`,
         type: `list`,
         message: "Choose your license:",
-        choices: [`Apache`,`Eclipse`,`GNU`,`MIT`]
-            // switch () {
-            //         case 'Apache':
-            //             return += ("[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
-            //             break;
-            //         case 'Eclipse':
-            //             return += ("[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)");
-            //             break;
-            //         case 'GNU':
-            //             return += ("[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)");
-            //             break;
-            //         case 'MIT':
-            //             return += ("[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
-            //             break;
-            //         default:
+        choices: [`Apache`,`Eclipse`,`GNU`,`MIT`],
+       
     },
 
   ])
@@ -77,6 +64,7 @@ inquirer
     }
   
   });
+
 
 
 });
